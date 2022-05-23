a0208e4b97c0747b2bad077213117afa315be405
LLIaluH <drunya2999@mail.ru>
2022-05-23 08:12:56 +0300

    Fix

--- NewArrChars/MaxSubsequence.cs
+++ NewArrChars/MaxSubsequence.cs
@@ -9,10 +9,10 @@ namespace NewArrChars
         readonly List<Word> Words;
         List<Sequence> maxCentrals; //самые длинные центральные последовательности
         Dictionary<char, List<Sequence>> ListSeqDict = new Dictionary<char, List<Sequence>>(); //хранит сумму длин последовательностей для каждого символа
-        //Dictionary<char, Sequens> MaxStartSeqDict = new Dictionary<char, Sequens>(); //хранит максимальную последовательность начала для каждого символа
-        //Dictionary<char, Sequens> MaxEndSeqDict = new Dictionary<char, Sequens>(); //хранит максимальную последовательность конца для каждого символа       
-        Dictionary<char, List<Sequence>> StartSeqDict = new Dictionary<char, List<Sequence>>(); //хранит максимальную последовательность начала для каждого символа
-        Dictionary<char, List<Sequence>> EndSeqDict = new Dictionary<char, List<Sequence>>(); //хранит максимальную последовательность конца для каждого символа
+        Dictionary<char, Sequence> MaxStartSeqDict = new Dictionary<char, Sequence>(); //хранит максимальную последовательность начала для каждого символа
+        Dictionary<char, Sequence> MaxEndSeqDict = new Dictionary<char, Sequence>(); //хранит максимальную последовательность конца для каждого символа       
+        //Dictionary<char, List<Sequence>> StartSeqDict = new Dictionary<char, List<Sequence>>(); //хранит максимальную последовательность начала для каждого символа
+        //Dictionary<char, List<Sequence>> EndSeqDict = new Dictionary<char, List<Sequence>>(); //хранит максимальную последовательность конца для каждого символа
 
         List<List<Sequence>> finalMax = new List<List<Sequence>>();
 
@@ -24,8 +24,8 @@ namespace NewArrChars
             for (int i = 0; i < 26; i++)//заполнение значениями поумолчанию
             {
                 ListSeqDict.Add((char)(i + 97), new List<Sequence>());
-                StartSeqDict.Add((char)(i + 97), null);
-                EndSeqDict.Add((char)(i + 97), null);
+                MaxStartSeqDict.Add((char)(i + 97), null);
+                MaxEndSeqDict.Add((char)(i + 97), null);
             }
             Words = words;
             maxCentrals = new List<Sequence>();
@@ -46,43 +46,68 @@ namespace NewArrChars
                 }
                 else
                 {
-                    char firstSimbol = currentWord.subSequenses[0].Simbol;
-                    char lastSimbol = currentWord.subSequenses[currentWord.subSequenses.Count - 1].Simbol;
-                    StartSeqDict[firstSimbol].Add(currentWord.subSequenses[0]);
-                    EndSeqDict[lastSimbol].Add(currentWord.subSequenses[currentWord.subSequenses.Count - 1]);
                     //char firstSimbol = currentWord.subSequenses[0].Simbol;
                     //char lastSimbol = currentWord.subSequenses[currentWord.subSequenses.Count - 1].Simbol;
-                    //int firstSimCount = currentWord.subSequenses[0].Count;
-                    //int lastSimCount = currentWord.subSequenses[currentWord.subSequenses.Count - 1].Count;
-                    //if (firstSimbol == lastSimbol)
-                    //{
-                    //    if (firstSimCount < lastSimbol)
-                    //    {
-                    //        if (MaxEndSeqDict[lastSimbol] == null || MaxEndSeqDict[lastSimbol].Count < lastSimCount)//конец
-                    //        {
-                    //            MaxEndSeqDict[lastSimbol] = currentWord.subSequenses[currentWord.subSequenses.Count - 1];
-                    //        }
-                    //    }
-                    //    else
-                    //    {
-                    //        if (MaxStartSeqDict[firstSimbol] == null || MaxStartSeqDict[firstSimbol].Count < firstSimCount)//начало
-                    //        {
-                    //            MaxStartSeqDict[firstSimbol] = currentWord.subSequenses[0];
-                    //        }
-                    //    }
-                    //}
-                    //else
-                    //{
-                    //    if (MaxStartSeqDict[firstSimbol] == null || MaxStartSeqDict[firstSimbol].Count < firstSimCount)//начало
-                    //    {
-                    //        MaxStartSeqDict[firstSimbol] = currentWord.subSequenses[0];
-                    //    }
-                    //    if (MaxEndSeqDict[lastSimbol] == null
-                    //        || MaxEndSeqDict[lastSimbol].Count < lastSimCount)//конец
-                    //    {
-                    //        MaxEndSeqDict[lastSimbol] = currentWord.subSequenses[currentWord.subSequenses.Count - 1];
-                    //    }
-                    //}
+                    //StartSeqDict[firstSimbol].Add(currentWord.subSequenses[0]);
+                    //EndSeqDict[lastSimbol].Add(currentWord.subSequenses[currentWord.subSequenses.Count - 1]);
+                    char firstSimbol = currentWord.subSequenses[0].Simbol;
+                    char lastSimbol = currentWord.subSequenses[currentWord.subSequenses.Count - 1].Simbol;
+                    int firstSimCount = currentWord.subSequenses[0].Count;
+                    int lastSimCount = currentWord.subSequenses[currentWord.subSequenses.Count - 1].Count;
+                    if (firstSimbol == lastSimbol)
+                    {
+                        if (firstSimCount < lastSimCount)
+                        {
+                            if (MaxEndSeqDict[lastSimbol] == null || MaxEndSeqDict[lastSimbol].Count < lastSimCount)//конец
+                            {
+                                MaxEndSeqDict[lastSimbol] = currentWord.subSequenses[currentWord.subSequenses.Count - 1];
+                            }
+                        }
+                        else if (firstSimCount > lastSimCount)
+                        {
+                            if (MaxStartSeqDict[firstSimbol] == null || MaxStartSeqDict[firstSimbol].Count < firstSimCount)//начало
+                            {
+                                MaxStartSeqDict[firstSimbol] = currentWord.subSequenses[0];
+                            }
+                        }
+                        else
+                        {
+                            if (MaxEndSeqDict[lastSimbol] == null)
+                            {
+                                MaxEndSeqDict[lastSimbol] = currentWord.subSequenses[currentWord.subSequenses.Count - 1];
+                            }
+                            else if (MaxStartSeqDict[firstSimbol] == null)
+                            {
+                                MaxStartSeqDict[firstSimbol] = currentWord.subSequenses[0];
+                            }
+                            else if (MaxStartSeqDict[firstSimbol].Count != MaxEndSeqDict[lastSimbol].Count)
+                            {
+                                if (MaxStartSeqDict[firstSimbol].Count > MaxEndSeqDict[lastSimbol].Count)
+                                {
+                                    MaxEndSeqDict[lastSimbol] = currentWord.subSequenses[currentWord.subSequenses.Count - 1];
+                                }
+                                else
+                                {
+                                    MaxStartSeqDict[firstSimbol] = currentWord.subSequenses[0];
+                                }
+                            }
+                            else if (MaxStartSeqDict[firstSimbol].Count == MaxEndSeqDict[lastSimbol].Count)//начало
+                            {
+                                MaxStartSeqDict[firstSimbol] = currentWord.subSequenses[0];
+                            }
+                        }
+                    }
+                    else
+                    {
+                        if (MaxStartSeqDict[firstSimbol] == null || MaxStartSeqDict[firstSimbol].Count < firstSimCount)//начало
+                        {
+                            MaxStartSeqDict[firstSimbol] = currentWord.subSequenses[0];
+                        }
+                        if (MaxEndSeqDict[lastSimbol] == null || MaxEndSeqDict[lastSimbol].Count < lastSimCount)//конец
+                        {
+                            MaxEndSeqDict[lastSimbol] = currentWord.subSequenses[currentWord.subSequenses.Count - 1];
+                        }
+                    }
                 }
 
                 for (int j = 0; j < currentWord.subSequenses.Count; j++)
@@ -115,14 +140,10 @@ namespace NewArrChars
                 List<Sequence> tempSeq = new List<Sequence>();
                 var currSim = (char)(i + 97);
                 int currCount = 0;//максимальная найденная длина последовательности
-                if (EndSeqDict[currSim] != null)
+                if (MaxEndSeqDict[currSim] != null)
                 {
-                    foreach (var sequences in EndSeqDict[currSim])
-                    {
-
-                    }
-                    //currCount += EndSeqDict[currSim].Count;
-                    //tempSeq.Add(EndSeqDict[currSim]);//добавляем в результующую последовательность самую максимальную конечную в начало
+                    currCount += MaxEndSeqDict[currSim].Count;
+                    tempSeq.Add(MaxEndSeqDict[currSim]);//добавляем в результующую последовательность самую максимальную конечную в начало
                 }
 
                 foreach (var currSeq in ListSeqDict[currSim])
@@ -131,10 +152,10 @@ namespace NewArrChars
                     tempSeq.Add(currSeq);
                 }
 
-                if (StartSeqDict[currSim] != null)
+                if (MaxStartSeqDict[currSim] != null)
                 {
-                    //currCount += StartSeqDict[currSim].Count;
-                    //tempSeq.Add(StartSeqDict[currSim]);//добавляем в результующую последовательность самую максимальную начальную в конец
+                    currCount += MaxStartSeqDict[currSim].Count;
+                    tempSeq.Add(MaxStartSeqDict[currSim]);//добавляем в результующую последовательность самую максимальную начальную в конец
                 }
 
                 if (lastMaxCount < currCount)
